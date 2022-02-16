f0b672a2770d4fcfe4a6d45a95e42ef82d8fec11
rh-cpranava <81606179+rh-cpranava@users.noreply.github.com>
2022-02-16 13:31:09 +0800

    Update cert-expire.sh

--- cert-expire.sh
+++ cert-expire.sh
@@ -39,9 +39,7 @@ echo "------------------------- all service serving cert secrets TLS certificate
 oc get secrets -A -o go-template='{{range .items}}{{if eq .type "kubernetes.io/tls"}}{{.metadata.namespace}}{{" "}}{{.metadata.name}}{{" "}}{{index .data "tls.crt"}}{{"\n"}}{{end}}{{end}}' | while read namespace name cert; do echo -en "$namespace\t$name\t"; echo $cert | base64 -d | show_cert; done
 
 
-## Process all cert files under /etc/kubernetes directories --> Master node
-### The following sections
-### Script execution requires sudo access to all nodes
+## Process all cert files under /etc/kubernetes directories in the Master node
 echo "------------------------- all nodes' kubelet TLS certificate -------------------------"
 for node in `oc get nodes |awk 'NR>1'|grep master | awk '{print $1}'`; do
     oc debug -q node/$node -- sh -c 'tar --transform "s/.*\///g" -zcf /host/tmp/etcd.tar.gz $(ls -A /host/etc/kubernetes/static-pod-resources/etcd-*/secrets/*/*.crt) 2>/dev/null '
